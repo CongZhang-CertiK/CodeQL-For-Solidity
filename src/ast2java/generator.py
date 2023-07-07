09e6051862a4658216b62262b6b483ffd0f6bab0
cong.zhang <cong.zhang@certik.com>
2023-07-07 17:59:49 +0800

    add compilation unit reorg layer

--- src/ast2java/generator.py
+++ src/ast2java/generator.py
@@ -5,21 +5,33 @@ from src.config import CONFIG
 
 def gen_java_from_ast(source_unit):
     copy_builtins_to_dist()
-    current_source_file = None
+    compilation_units = scatter_to_compilation_units(source_unit)
+    for compilation_unit in compilation_units:
+        pragma_directive, contract_definition = compilation_unit
+        current_source_file = JavaSourceFile()
+        current_source_file.pragma_info = f"// pragma {pragma_directive.get('name')} {pragma_directive.get('value')};"
+        current_source_file.ast = contract_definition
+        current_source_file.update(contract_definition)
+        current_source_file.write_to_file()
+
+
+def scatter_to_compilation_units(source_unit):
+    compilation_units = []
+    pragma_directive = None
     for ast in source_unit.get('children'):
-        if ast.get('type') == "PragmaDirective" and current_source_file is None:
-            current_source_file = JavaSourceFile()
-            current_source_file.pragma_info = f"// pragma {ast.get('name')} {ast.get('value')};"
-            continue
-        elif ast.get('type') == "ContractDefinition" and current_source_file is not None:
-            current_source_file.ast = ast
-            current_source_file.update(ast)
-            current_source_file.write_to_file()
-            current_source_file = None
+        if ast.get('type') == "PragmaDirective" and pragma_directive is None:
+            pragma_directive = ast
+        elif ast.get('type') == "ContractDefinition" and pragma_directive is not None:
+            compilation_units.append((pragma_directive, ast))
+            pragma_directive = None
         else:
-            logger.debug("[Unexpected Control Flow]")
-            logger.debug("Current Source File: ", current_source_file)
-            logger.debug("AST Node Type: ", ast.get('type'))
+            logger.debug("unknown source unit children type.")
+    return reorg_compilation_tree(compilation_units)
+
+
+def reorg_compilation_tree(compilation_units):
+    reorged = compilation_units
+    return reorged
 
 
 def copy_builtins_to_dist():
